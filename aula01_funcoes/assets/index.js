bf96e3f077d2c71f5f450a7fe657f26483ab66c4
jeiel2013 <contatojeiel2013@gmail.com>
2024-03-16 11:45:52 -0300

    16/03/24

---
+++ aula01_funcoes/assets/index.js
@@ -0,0 +1,7 @@
+function sayHello(name) {
+    console.log(`Olá ${name}. Bem vindo(a) à Dev Samurai!`);
+}
+
+sayHello('Felipe');
+sayHello('Paulo');
+sayHello('Marcelo');
---
+++ aula02_retorno/assets/index.js
@@ -0,0 +1,7 @@
+function sum(n1, n2) {
+    // const result = n1 + n2;
+    // return result;
+    return n1 + n2;
+}
+
+console.log(sum(1, 1));
